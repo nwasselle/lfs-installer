d01fc35812709deba38917a789fd477601fe356d
Nicholas Wasselle <89424812+nwasselle@users.noreply.github.com>
2022-07-06 12:03:01 -0700

    Install host system requirements

--- install.sh
+++ install.sh
@@ -23,6 +23,39 @@ export TGTDSK="/dev/sda"
 ####################################################################
 
 # -----------------------------------------------------------------
+# Install the Host System Requirements
+# -----------------------------------------------------------------
+
+# Install Packages in order
+sudo apt install bash -y
+sudo apt install binutils -y
+sudo apt install bison -y
+sudo apt install coreutils -y
+sudo apt install diffutils -y
+sudo apt install findutils -y
+sudo apt install gawk -y
+sudo apt install gcc -y
+sudo apt install grep -y
+sudo apt install gzip -y
+sudo apt install m4 -y
+sudo apt install make -y
+sudo apt install patch -y
+sudo apt install perl -y
+sudo apt install python3 -y
+sudo apt install sed -y
+sudo apt install tar -y
+sudo apt install texinfo -y
+sudo apt install xz -y
+
+# Verify kernel version
+# uname -r
+
+# Make required symlinks
+ln -sf /bin/sh /usr/bin/bash
+ln -sf /usr/bin/yacc /usr/bin/bison
+ln -sf /usr/bin/awk /usr/bin/gawk
+
+# -----------------------------------------------------------------
 # Partition the Hard Drive
 # -----------------------------------------------------------------
 
@@ -52,7 +85,6 @@ EOF
 # -----------------------------------------------------------------
 # Create filesystems
 # -----------------------------------------------------------------
-
 
 # Initialize swap partition
 sudo mkswap /dev/sda1
