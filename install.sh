e8ad7eb8a540397213a92b7259254fd91c39433e
Nicholas Wasselle <89424812+nwasselle@users.noreply.github.com>
2022-07-30 19:45:15 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -30,33 +30,14 @@ export LFS_TGT="/mnt/lfs/tools"
 # -----------------------------------------------------------------
 
 # Install Packages in order
-sudo apt install bash -y
-sudo apt install binutils -y
-sudo apt install bison -y
-sudo apt install coreutils -y
-sudo apt install diffutils -y
-sudo apt install findutils -y
-sudo apt install gawk -y
-sudo apt install gcc -y
-sudo apt install grep -y
-sudo apt install gzip -y
-sudo apt install m4 -y
-sudo apt install make -y
-sudo apt install patch -y
-sudo apt install perl -y
-sudo apt install python3 -y
-sudo apt install sed -y
-sudo apt install tar -y
-sudo apt install texinfo -y
-sudo apt install xz -y
+bash req-install.sh
+
 
 # Verify kernel version
 # uname -r
 
 # Make required symlinks
-sudo ln -sf /bin/sh /usr/bin/bash
-sudo ln -sf /usr/bin/yacc /usr/bin/bison
-sudo ln -sf /usr/bin/awk /usr/bin/gawk
+bash req-linker.sh
 
 # -----------------------------------------------------------------
 # Partition the Hard Drive
