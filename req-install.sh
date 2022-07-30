a92e3e09304146cacb8c123c7c899ec9ab9bc3ea
Nicholas Wasselle <89424812+nwasselle@users.noreply.github.com>
2022-07-30 19:29:52 -0400

    Update req-install.sh

--- req-install.sh
+++ req-install.sh
@@ -1,20 +1,3 @@
 #!/bin/sh
-sudo apt install bash -y
-sudo apt install binutils -y
-sudo apt install bison -y
-sudo apt install coreutils -y
-sudo apt install diffutils -y
-sudo apt install findutils -y
-sudo apt install gawk -y
-sudo apt install gcc -y
-sudo apt install grep -y
-sudo apt install gzip -y
-sudo apt install m4 -y
-sudo apt install make -y
-sudo apt install patch -y
-sudo apt install perl -y
-sudo apt install python3 -y
-sudo apt install sed -y
-sudo apt install tar -y
-sudo apt install texinfo -y
+sudo apt install $(cat pkg-list) -y
 
